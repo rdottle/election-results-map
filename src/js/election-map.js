adb78d4072edc3ac56f9506999f1f5f9ec31a68f
rdottle <rachaelcdottle@gmail.com>
2018-11-09 16:07:18 -0500

    Add topojson and include

--- src/js/election-map.js
+++ src/js/election-map.js
@@ -1,7 +1,11 @@
+import { feature } from 'topojson-client';
 
 class Map {
 	constructor(opts) {
 		this.year = opts.year;
+		this.data = opts.data;
+		this.topo = opts.us;
+		this.parent = opts.parent;
 	}
 
 	init () {
@@ -10,6 +14,12 @@ class Map {
 
 	draw () {
 		console.log(this.year);
+		console.log(this.data);
+	}
+
+	addMap () {
+		this.svg = this.parent.append('svg');
+		this.states = feature(this.topoUS, this.topoUS.objects.us);
 	}
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,22 @@
 'use strict';
 import { json } from 'd3-fetch';
+import { select } from 'd3-selection';
+// import { geoPath } from 'd3-geo';
 import { Map } from '../../src/js/election-map.js';
-
+const us = require('../../src/data/us.topo.json');
 
 var year = '2008';
 var url = `https://raw.githubusercontent.com/TimeMagazine/presidential-election-results/master/data/results_${year}.json`;
 
-console.log('this is working');
-
+console.log(us);
+const mapContainer = select('.map');
+console.log(mapContainer);
 json(url).then(function (data) {
-    console.log(data);
     const electionMap = new Map({
-	    year: year
+	    year: year,
+	    data: data,
+	    us: us,
+	    parent: mapContainer 
     });
 
     electionMap.init();
