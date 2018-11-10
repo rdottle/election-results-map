280da02c6e65bed484e80a99a788ce270f6383ab
rdottle <rachaelcdottle@gmail.com>
2018-11-10 18:05:43 -0500

    Start drawing us map features

--- src/js/election-map.js
+++ src/js/election-map.js
@@ -1,4 +1,8 @@
 import { feature } from 'topojson-client';
+import { select, append } from 'd3-selection';
+import { transition } from 'd3-transition';
+import { geoPath, geo } from 'd3-geo';
+import * as d3 from 'd3'; 
 
 class Map {
 	constructor(opts) {
@@ -6,6 +10,12 @@ class Map {
 		this.data = opts.data;
 		this.topo = opts.us;
 		this.parent = opts.parent;
+
+		this.projection = d3.geoAlbers()
+				.translate([1400/2, 700/2])    
+				.scale([1000]);
+
+		this.path = d3.geoPath();          
 	}
 
 	init () {
@@ -13,14 +23,30 @@ class Map {
 	}
 
 	draw () {
-		console.log(this.year);
-		console.log(this.data);
+		this.addMap();
 	}
 
 	addMap () {
-		this.svg = this.parent.append('svg');
-		this.states = feature(this.topoUS, this.topoUS.objects.us);
+		this.svg = this.parent.append('svg')
+		 				.attr('width', 1400)
+		 				.attr('height', 700)
+		this.states = feature(this.topo, this.topo.objects.us);
+
+		console.log(this.states.features);
+
+	    this.svg.selectAll("path")
+				.data(this.states.features)
+				.enter()
+					.append("path")
+					.attr('class', d => {
+						return d.properties.STUSPS
+					})
+					.attr("d", this.path)
+					.attr("transform", "scale(" + 0.0007 + ")")
+					.style("stroke", "#fff")
+					.style("stroke-width", "1");
 	}
+
 }
 
 export { Map }; 
